cb13eea33e2a1d2ddc2e0a841630fd7ac5b215d2
Konrad Kreczko <konradkreczko@gmail.com>
2023-10-16 22:59:30 +0200

    feat: Created working calculator logic with buttons

--- src/main.js
+++ src/main.js
@@ -8,7 +8,10 @@ const numButtons = document.querySelectorAll(".num-button");
 const deleteButton = document.querySelector("#delete-button");
 const operationButtons = document.querySelectorAll(".operation-button");
 const mainDisplay = document.querySelector("#mid-disp-content");
-const upperDisplay = document.querySelector("#upper-disp-content");
+let memory = 0
+let left = null;
+let currentOperator = "+";
+let right = null;
 
 numButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -18,12 +21,41 @@ numButtons.forEach((button) => {
 
 deleteButton.addEventListener("click", () => {
   emptyMainDisplay();
-  emptyUpperDisplay();a
+  emptyUpperDisplay();
+  memory = 0;
+  left = null;
+  right = null;
 })
+
 
 operationButtons.forEach((button) => {
   button.addEventListener(("click"), () => {
-    populateUpperDisplay(mainDisplay.textContent + button.id);
-    emptyMainDisplay()
+    if (left === null) {
+      left = +mainDisplay.textContent;
+    } else if (right === null) {
+      right = +mainDisplay.textContent;
+      memory = operate(left, currentOperator, right);
+      emptyMainDisplay();
+      emptyUpperDisplay();
+      populateMainDisplay(memory);
+      left = memory
+      right = null;
+    }
+
+    currentOperator = button.id;
+    populateUpperDisplay(left + currentOperator);
+    emptyMainDisplay();
   })
+})
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener(("click"), () => {
+  right = +mainDisplay.textContent;
+  memory = operate(left, currentOperator, right);
+  emptyMainDisplay();
+  emptyUpperDisplay();
+  populateMainDisplay(memory);
+  memory = 0
+  left = null
+  right = null
 })
